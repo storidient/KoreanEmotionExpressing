29a353ec91e5b82943864c1eb0ade007188971f0
Storidient <83331340+storidient@users.noreply.github.com>
2022-08-11 11:28:42 +0900

    Update utils.py

--- data/utils.py
+++ data/utils.py
@@ -1,6 +1,8 @@
 import re
-from rx_codes import *
+from rx_codes import chinese_rx, blank_chinese, katakana_middle
 from cached_property import cached_property
+from typing import List, Dict, Optional
+from itertools import product
 
 class CleanWord:
   """Delete all the unneccessary marks in a word
@@ -36,7 +38,7 @@ class CleanWord:
   
 class Options:
   """
-  Change a representation with [/] into a list of all the possible forms
+  Change the representation form with [/] into a list of all the possible forms
   
   Attributes: 
     input : a representation with [', ']' and '/' (e.g. 밥[빵/국]을 먹다)
@@ -73,3 +75,64 @@ class Options:
       
       self.output.append(possible_form)
 
+      
+      
+      
+class ReviseRep(CleanWord):
+  def __init__(self, save_options : bool = True):
+    super().__init__()
+    self.save_options = save_options
+
+  def space_option(self, word : str, options : Optional[List[str]] = None):
+    """Change '^' into space or Delete '^' mark"""
+    rep = re.sub('\^', ' ', word) #change into space
+    with_space = re.sub('\^', '', word) #delete ^ mark
+    if rep != word and self.save_options == True:
+      options += [rep, with_space]
+    return rep, options
+  
+  def word_option(self, phrase : str, options : Optional[List[str]] = None):
+    """Delete '[Option1/Option2]' in the representation form"""
+    rep = re.sub('\[[^\]]+\]', '', phrase)
+    if self.save_options == True:
+      if len(options) == 0:
+        options.append(phrase)
+      
+      options = sum(list(map(lambda x : Options(x).output, options)), [])
+    return rep, list(set(options))
+
+  def josa_option(self, word : str, options : Optional[List[str]] = None):
+    """Delete '(Option)' in the representation form (e.g. 밥(을) 먹다)"""
+    rep = re.sub('\(.*\)', '', word)    
+    
+    if self.save_options == True:
+      if len(options) == 0:
+        options.append(word)
+      
+      without_josa = list(map(lambda x : re.sub('\(.*\)', '', x), options))
+      with_josa = list(map(lambda x : re.sub('[\(\)]', '', x), options))
+      options = without_josa + with_josa
+    
+    return rep, options
+
+  def main(self, word : str) -> str:
+    """revise word represetation form with all the rules"""
+    rep = self.del_all(word)
+    options = list() if self.save_options == True else None
+
+    if re.match('.*\^', rep): #delete ^
+      rep, options = self.space_option(rep, options)
+
+    if re.match('.*\[.*\]', rep): #delete 
+      rep, options = self.word_option(rep, options)
+
+    if re.match('.*\(.*\)', rep):
+      rep, options = self.josa_option(rep,options)
+    
+    rep = self.del_space(rep)
+    
+    if self.save_options == True:
+      options = list(map(self.del_space, options))
+      options.append(rep)
+
+    return rep, options
