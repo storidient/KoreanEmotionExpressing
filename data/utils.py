03ae7b9aa451dfced70c525af4450bb7c6891f16
Storidient <83331340+storidient@users.noreply.github.com>
2022-08-11 11:32:04 +0900

    Create utils.py

---
+++ data/utils.py
@@ -0,0 +1,42 @@
+from cached_property import cached_property
+from typing import List
+from itertools import product
+
+class Options:
+  """
+  Change the representation form with [/] into a list of all the possible forms
+  
+  Attributes: 
+    input : a representation with [', ']' and '/' (e.g. 밥[빵/국]을 먹다)
+    output : a list of all the possible forms (e.g.['밥을 먹다', '빵을 먹다, '국을 먹다'])
+  """
+  def __init__(self, phrase : str):
+    self.input, self.output = phrase, list()
+    self._build()
+  
+  @cached_property
+  def targets(self):
+    """Returns a range matched with 'OptionOne[OptionTwo/OptionThree]'"""
+    return re.findall('[^ ]*\[[^\]]+\]', self.input)
+
+  @cached_property
+  def options(self):
+    """Returns a list of options"""
+    return list(map(self.split_option, self.targets))
+
+  def split_option(self, target : str) -> List[str]:
+    """Change a string with [] into a list"""
+    items = re.split('[\[\/]', re.sub('\]', '', target))
+    return [x for x in items if len(x.strip(' ')) > 0]
+  
+  def _build(self):
+    all_combination = list(product(*self.options))
+    
+    for option_set in all_combination:
+      possible_form = '' + self.input #copy the original form
+
+      for idx, target in enumerate(self.targets):
+        target = re.sub('\]', '\]', re.sub('\[', '\[', target))
+        possible_form = re.sub(target, option_set[idx], possible_form)
+      
+      self.output.append(possible_form)
