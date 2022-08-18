fa6cc58899961280a46c4e92ae7a4e3c67e5b4a1
Storidient <83331340+storidient@users.noreply.github.com>
2022-08-18 19:57:59 +0900

    Update preprocessing.py

--- data/preprocessing.py
+++ data/preprocessing.py
@@ -86,6 +86,7 @@ class ReviseDef(CleanWord):
       without_num = re.sub('[0-9]', '', target)
       
       if without_num != target:
+        print(item, target, without_num)
         item = re.sub(target, without_num, item)
     
     return re.sub('[\[「][0-9]*[\]」]', '', item)
