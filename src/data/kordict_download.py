c34ab3d78603467628c9ede7438be609495b6655
Storidient <83331340+storidient@users.noreply.github.com>
2022-08-27 19:44:08 +0900

    Update kordict_download.py

--- src/data/kordict_download.py
+++ src/data/kordict_download.py
@@ -1,5 +1,6 @@
 import json
 from typing import Dict, List
+from jamo import h2j, j2hcj
 
 
 
@@ -9,7 +10,7 @@ def extract_conjugation(item : List[Dict[str, str]]) -> List[str]:
   avoid_zero = list(filter(lambda x: len(x) > 0, 
                             list(map(lambda x: x.strip(' '), conjugation))
                             ))
-  return list(filter(lambda x: x[-1] not in ['니', '오', '는', '지', '고', '소'], avoid_zero))
+  return list(filter(lambda x: j2hcj(h2j(x[-1]))[-1] not in ['ㄴ', 'ㅣ','ㅗ'], avoid_zero))
 
 
 class OpenKorean:
