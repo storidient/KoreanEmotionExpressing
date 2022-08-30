829a5923019af9a8d969fa5f944dfa77b930f0d8
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-08-30 20:26:34 +0900

    Update kordict_preprocess.py

--- src/data/kordict_preprocess.py
+++ src/data/kordict_preprocess.py
@@ -83,7 +83,8 @@ class ReviseDef(CleanWord):
   def clean_rep(self, item : str) -> str:
     """delete numbers with the word representation
     (e.g. '‘단어01’의 준말')"""
-    targets = [_ for _ in re.findall("‘[^’]*’", item) if len(re.findall(self.chinese_rx, _)) == 0]
+    word_reps = re.findall("‘[^’]*’", item)
+    targets = [_ for _ in word_reps if len(re.findall(self.chinese_rx, _)) == 0]#exclude chinese letter
       
     for target in targets:
       revised = self.clean_rep(target)[0]
