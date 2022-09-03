70815fbd7a6a9d12c0ef32c1ee2a17ff79f69543
Storidient <83331340+storidient@users.noreply.github.com>
2022-09-03 09:15:51 +0900

    Update rx_codes.py

--- data/rx_codes.py
+++ data/rx_codes.py
@@ -1,20 +1,61 @@
 import re
-from typing import List, Tuple
+from typing import List, Tuple, Union
+from attr import define
+from cached_property import cached_property
 
-def build_rx(unicode_list : List[Tuple[str, str]]) -> str:
-  """compile regular expression with unicodes"""
-  unicodes = ''.join(['%s-%s' % (s,t) for s, t in unicode_list])
-  return '[' + unicodes +']'
+@define
+class B:
+  start : str
+  end : str
 
-old_korean_unicode = [('\u3164', '\u318c'),
-                      ('\u318e', '\u318f'), 
-                      ('\ua960', '\ua97f'),
-                      ('\ud7b0', '\ud7ff'),
-                      ('\ue000', '\uefff'),
-                      ('\uf000', '\uffff'),
-                      ('\u1113', '\u115f'),
-                      ('\u1176', '\u11a7'),
-                      ('\u11c3', '\u11ff')]
+class Brackets:
+  b_small = B('\(', '\)')
+  b_inequal_a = B('<', '>')
+  b_inequal_b = B('〈','〉')
+  b_middle_a = B('\[','\]')
+  b_middle_b = B('〔', '〕')
+  b_big = B('{', '}')
+  b_sickle = B('「', '」')
+  b_double_sicke = B('『','』')
+  b_double_inequal = B('《', '》')
+
+  @classmethod
+  def search(cls, mark_name):
+    result = {k : v for k,v in cls.__dict__.items() if mark_name in k}
+    if len(result) == 0:
+      return None
+    elif len(result) > 1:
+      return result
+    else:
+      return list(result.values())[0]
+  
+  @classmethod
+  def ends(cls):
+    return [v.end for k,v in cls.__dict__.items() if k.startswith('b')]
+
+  @classmethod
+  def starts(cls):
+    return [v.start for k,v in cls.__dict__.items() if k.startswith('b')]
+  
+  @classmethod
+  def get_end(cls, start):
+    output = [cls.end()[idx] for idx, item in enumerate(cls.start()) if item == start]
+    return output[0] if len(output) > 0 else None 
+  
+  @classmethod
+  def get_start(cls, end):
+    output = [cls.start()[idx] for idx, item in enumerate(cls.end()) if item == end]
+    return output[0] if len(output) > 0 else None
+  
+old_kor_unicode = [('\u3164', '\u318c'),
+                   ('\u318e', '\u318f'), 
+                   ('\ua960', '\ua97f'),
+                   ('\ud7b0', '\ud7ff'),
+                   ('\ue000', '\uefff'),
+                   ('\uf000', '\uffff'),
+                   ('\u1113', '\u115f'),
+                   ('\u1176', '\u11a7'),
+                   ('\u11c3', '\u11ff')]
 
 chinese_unicode = [('\u31c0', '\u31ef'),
                    ('\u31f0', '\u31ff'),
@@ -25,17 +66,59 @@ chinese_unicode = [('\u31c0', '\u31ef'),
                    ('\u4e00', '\u9fff'),
                    ('\uf900', '\ufaff')]
 
-roman_num = [('\u2160', '\u217f')]
-line_pattern = '\"[^"]+"|\'[^\']+\''
-indirect_pattern = ' ?((이?라|하)?[고|며|면서]|[라란] |하[는니였]|[한하](다| ?뒤)|할 )'
+roman_num_unicode = [('\u2160', '\u217f')]
 
-old_kor_rx = re.compile('.*' + build_rx(old_korean_unicode), re.UNICODE)
-chinese_rx = re.compile('[\[\(]?' + build_rx(chinese_unicode) + '[\]\)]?', re.UNICODE)
-blank_chinese= re.compile('[\u3000]', re.UNICODE)
-katakana_middle = re.compile('[\u30fb]', re.UNICODE)
-roman_num_rx = build_rx(roman_num)
-roman_bracket = re.compile('[\(\[]' + roman_num_rx + '+[\]\)]', re.UNICODE)
-line_rx = re.compile(line_pattern)
-end_rx = re.compile('[\.\!\?]')
-indirect_rx = re.compile(indirect_pattern)
-after_indirect_rx = re.compile('(' + line_pattern + ')' + indirect_pattern)
+class RxCodes:
+  def __init__(self):
+    self.bracket = Brackets
+    self.line = '\"[^"]+"|\'[^\']+\''
+    self.indirect = ' ?((이?라|하)?[고|며|면서]|[라란] |하[는니였]|[한하](다| ?뒤)|할 )'
+    self.end = '[\.\!\?]'
+    self.chinese = self.rx_string(chinese_unicode)
+    self.old_kor = self.rx_string(old_kor_unicode)
+    self.roman_num = self.rx_string(roman_num_unicode)
+    self.b_start = '[' + ''.join(Brackets.starts()) + ']'
+    self.b_end = '[' + ''.join(Brackets.ends()) + ']'
+    self.blank_chinese = self.build_rx('[\u3000]')
+    self.katakan_middle = self.build_rx('[\u30fb]')
+
+  @staticmethod
+  def rx_string(unicode_list : List[Tuple[str, str]]) -> str:
+    """Return regular expression string"""
+    unicodes = ''.join(['%s-%s' % (s,t) for s, t in unicode_list])
+    return '[' + unicodes +']'
+  
+  @staticmethod
+  def build_rx(rx_str : Union[str, List[str]]):
+    if type(rx_str) == list:
+      rx_str = ''.join(rx_str)
+    return re.compile(rx_str, re.UNICODE)
+
+  def search_attr(self, name):
+    return [v for k, v in self.__dict__.items() if name in k]
+
+  def __getattr__(self, name):
+    target = [v for k, v in self.__dict__.items() if re.sub('_all', '', name) in k]
+      
+    if name.endswith('_all'):
+      target = self.search_attr(re.sub('_all', '', name))
+      return self.build_rx([self.b_start,'?',target[0], self.b_end, '?']) if len(target) > 0 else None
+    
+    elif name.endswith('_bracket'):
+      target = self.search_attr(re.sub('_bracket', '', name))
+      return self.build_rx([self.b_start,target[0], self.b_end]) if len(target) > 0 else None
+
+    elif name.endswith('_rx'):
+      target = self.search_attr(re.sub('_rx', '', name))
+      return self.build_rx(target) if len(target) > 0 else None
+    
+    elif name.endswith('_after'):
+      target = self.search_attr(re.sub('_after', '', name))
+      return self.build_rx('.*' + target) if len(target) > 0 else None
+
+    elif name.startswith('bracket_'):
+      target = re.sub('bracket_', '', name)
+      return self.bracket.search(target) if len(target) > 0 else None
+
+    else:
+      return None
