feb297dfe164d1aa10c55dc7f7bed827629b8fbe
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-09-14 08:59:56 +0900

    Update make_dataset.py

--- src/data/kordict/make_dataset.py
+++ src/data/kordict/make_dataset.py
@@ -1,7 +1,9 @@
 import json, re
 from typing import Dict, List, Optional
 from src.data.kordict.utils import Options
+from src.data.utils import OLD_KOR_UNICODE
 
+OLD_KOR = re.compile('['+ ''.join(['%s-%s' % (s,e) for s,e in OLD_KOR_UNICODE]) + ']', re.UNICODE)
 
 def get_conju(item : List[Dict[str, str]]) -> List[str]:
   """Return conjugation forms of a word"""
