fce16afbc33af44024407880c7a9badac71749cf
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-09-13 20:48:55 +0900

    Update utils.py

--- src/data/kordict/utils.py
+++ src/data/kordict/utils.py
@@ -1,7 +1,6 @@
 import re, unicodedata
 from collections import defaultdict
 from cached_property import cached_property
-from data.rx_codes import *
 from typing import List, Dict
 from itertools import product
 import numpy as np
@@ -51,83 +50,3 @@ class Options:
         possible_form = re.sub(target, option_set[idx], possible_form)
       
       self.output.append(possible_form)
-      
-      
-class CleanWord:
-  """Delete unneccessary marks in a word
-  Attributes:
-    chinese_rx : the regular expression of Chinese letters
-    blank_chinese : the regular expression of 'blank' in Chinese letters
-    katakana_middle : the regular expression of 'ㆍ' in Japanese(Katakana) letters
-  """
-  def __init__(self):
-    self.chinese_rx = chinese_rx
-    self.blank_chinese = blank_chinese
-    self.katakana_middle = katakana_middle
-    self.roman_bracket = roman_bracket
-    self.ch_with_bracket = re.compile('[\(\[]' + build_rx(chinese_unicode) + '+[\)\]]', re.UNICODE)
-    self.roman_bracket = roman_bracket
-  
-  def del_chinese(self, item : str) -> str:
-    """Delete the Chinese letters and empty brackets (e.g. '[]', '()')"""
-    return self.chinese_rx.sub('', item)
-  
-  def del_chinese_bracket(self, item : str) -> str:
-    """Delete the Chinese letters and empty brackets (e.g. '[]', '()')"""
-    return self.ch_with_bracket.sub('', item)
-  
-  def del_english(self, item : str) -> str:
-    """Delete the English letters and empty brackets (e.g. '[]', '()')"""
-    return re.sub('[\(\[][ A-Za-z]+[\(\]]', '', item)
-    
-  def del_space(self, item : str) -> str:
-    """Delete unneccessary spaces in a word"""
-    return re.sub(' +', ' ', item.strip())
-  
-  def del_all(self, word : str) -> str:
-    """Delete all the unneccessary marks
-    (e.g. Arabian numbers, under-bar, chinese letters, hyphen, end marks)
-    """
-    word = unicodedata.normalize('NFC', word)
-    without_chinese = self.blank_chinese.sub(' ', self.del_chinese(word))
-    without_katakana = self.katakana_middle.sub('ㆍ', without_chinese)   
-    without_roman = self.roman_bracket.sub('', without_katakana)
-    without_numbering = re.sub('[\[「]]?[0-9]+[\]」]?', '', without_roman)
-    
-    return re.sub('[\.,\-_]', '', without_numbering)
-
-
-class FilterWord:
-  """Filter the words
-
-    Attributes:
-      allow_old : allow old Korean letters
-      allow_broken : allow broken Korean letter
-  """
-  def __init__(self, 
-               allow_old : bool = False,
-               allow_broken : bool = False):
-    self.allow_old = allow_old
-    self.allow_broken = allow_broken #allow
-    self._build()
-  
-  def _build(self):
-    if self.allow_old == False:
-      from data.rx_codes import old_kor_rx
-      self.old_kor_rx = old_kor_rx
-
-    if self.allow_broken == False:
-      self.broken_kor_rx = re.compile('^[ㄱ-ㅎㅏ-ㅣ]')
-
-  def run(self, item : str) -> bool:
-    item = unicodedata.normalize('NFC', item)
-
-    if self.allow_old == False:
-      if self.old_kor_rx.match(item):
-        return False
-
-    if self.allow_broken == False:
-      if self.broken_kor_rx.match(item):
-        return False
-
-    return True
