07b6bc0571592f755549caaedb6dc8e78ac0e037
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-09-15 16:03:52 +0900

    Update make_dataset.py

--- src/data/kordict/make_dataset.py
+++ src/data/kordict/make_dataset.py
@@ -3,16 +3,8 @@ from typing import Dict, List, Union
 import numpy as np
 
 
-def get_conju(item : List[Dict[str, str]]) -> List[str]:
-  """Return conjugation forms of a word"""
-  return [{'long' : x['conjugation_info']['conjugation'],
-           'short' : x['abbreviation_info']['abbreviation'] if 'abbreviation_info' in x.keys() else None
-           } for x in item]
-
-
-class OpenKorean:
-  """Get word information from a json file downloaded from Open Korean Dictionary
-  (https://opendict.korean.go.kr/main)
+class KoreanDictionary:
+  """
   Attributes:
     path : a path of json file
     output : a list of dictionaries with word information
@@ -23,15 +15,41 @@ class OpenKorean:
         pos: a list of part-of-speech
         source : 'OKD(Open Korean Dictionary)'}
   """
-  def __init__(self, path : str):
-    with open(path, 'r') as f:
-      self.data = json.load(f)
-    self.output = self._build()
+  def __init__(self, data, standard : bool = True):
+    input = data['channel']['item']
+    self.output = list(map(self.standard_info, input)) if standard == True else list(map(self.our_info, input))
+  
+  @staticmethod
+  def get_conju(item : List[Dict[str, str]]) -> List[str]:
+    """Return conjugation forms of a word"""
+    return [{'long' : x['conjugation_info']['conjugation'],
+             'short' : x['abbreviation_info']['abbreviation'] if 'abbreviation_info' in x.keys() else None
+            } for x in item]
+  
+  @staticmethod
+  def standard_info(item) -> Dict[str, Union[List[str], str]]:
+    """Get word information from a json file downloaded from Standard Korean Dictionary
+    (https://stdict.korean.go.kr/main/main.do)"""
+    item = item['word_info']
+    item_pos = item['pos_info']
+    item_pattern = item_pos[0]['comm_pattern_info']
+    pos = item_pos[0]['pos']
+    pattern = [item_pattern[0]['pattern_info']['pattern']] if 'pattern_info' in item_pattern[0].keys() else list()
+    conjugation = get_conju(item['conju_info']) if ('형용사' in pos or '동사' in pos) and ('conju_info' in item.keys()) else list()
 
-  def _build(self) -> List[Dict[str, Union[List[str], str]]]:
-    return list(map(self.get_info, self.data['channel']['item']))
-    
-  def get_info(self, item) -> Dict[str, Union[List[str], str]]:
+    return {'word' : item['word'],
+            'word_unit' : item['word_unit'],
+            'pattern' : pattern,
+            'conjugation' : conjugation,
+            'pos' : pos,
+            'definition': [_['definition'] for _ in in item_pattern[0]['sense_info']],
+            'type' : '일반어',
+            'source' : 'SKD'}
+  
+  @staticmethod
+  def our_info(item) -> Dict[str, Union[List[str], str]]:
+    """Get word information from a json file downloaded from Open Korean Dictionary
+    (https://opendict.korean.go.kr/main)"""
     pos = item['senseinfo']['pos'] if 'pos' in item['senseinfo'].keys() else '품사 없음'
     pattern = [x['pattern'] for x in item['senseinfo']['pattern_info']] if 'pattern_info' in item['senseinfo'].keys() else list()
     conjugation = get_conju(item['wordinfo']['conju_info']) if ('형용사' in pos or '동사' in pos) and ('conju_info' in item['wordinfo'].keys()) else list()
@@ -40,46 +58,7 @@ class OpenKorean:
             'word_unit' : item['wordinfo']['word_unit'],
             'pattern' : pattern,
             'conjugation' : conjugation,
-            'definition' : item['senseinfo']['definition'],
+            'definition' : [item['senseinfo']['definition']],
             'type' : item['senseinfo']['type'],
             'pos' : pos,
             'source' : 'OKD'}
-
-  
-class StandardKorean:
-  """Get word information from a json file downloaded from Standard Korean Dictionary
-  (https://stdict.korean.go.kr/main/main.do)
-  Attributes:
-    path : a path of json file
-    output : a list of dictionaries with word information
-       {word : a list of representation form of words
-        word_unit : a list of 'unit' of words (e.g. 'phrase', 'word', 'saying')
-        definition : a list of definition of words 
-        type: 일반어(ordinary words)
-        pos: a list of part-of-speech
-        source : 'SKD(Standard Korean Dictionary)'}
-  """
-  def __init__(self, path : str):
-    with open(path, 'r') as f:
-      self.data = json.load(f)
-    self.output = self._build()
-    
-  def _build(self) -> List[Dict[str, Union[List[str], str]]]:
-    return sum(list(map(self.get_info, self.data['channel']['item'])),[])
-    
-  def get_info(self, item) -> Dict[str, Union[List[str], str]]:
-    item = item['word_info']
-    item_pos = item['pos_info']
-    item_pattern = item_pos[0]['comm_pattern_info']
-    pos = item_pos[0]['pos']
-    pattern = [item_pattern[0]['pattern_info']['pattern']] if 'pattern_info' in item_pattern[0].keys() else list()
-    conjugation = get_conju(item['conju_info']) if ('형용사' in pos or '동사' in pos) and ('conju_info' in item.keys()) else list()
-
-    return [{'word' : item['word'],
-             'word_unit' : item['word_unit'],
-             'pattern' : pattern,
-             'conjugation' : conjugation,
-             'pos' : pos,
-             'definition': sense_info['definition'],
-             'type' : '일반어',
-             'source' : 'SKD'} for sense_info in item_pattern[0]['sense_info']]
