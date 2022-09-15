c18ea34fe203dc476d192d81dd0d0b188ee17927
koreankiwi99 <77032780+koreankiwi99@users.noreply.github.com>
2022-09-15 11:46:37 +0900

    Create utils.py

---
+++ src/data/kordict/utils.py
@@ -0,0 +1,17 @@
+from src.data.utils import CleanStr
+from src.data.utils import OLD_KOR_UNICODE, ROMAN_NUM_UNICODE, CHINESE_UNICODE
+
+EOMI = 'ㅕㅓㅏㅑㅘㅝㅐㅒㅖㅔ'
+OLD_KOR = re.compile('.*'+'['+ ''.join(['%s-%s' % (s,e) for s,e in OLD_KOR_UNICODE]) + ']', re.UNICODE)
+NUMBERS =  '[' + '0-9' + ''.join(['%s-%s' % (s,e) for s,e in ROMAN_NUM_UNICODE]) + ']'
+CHINESE_ENGLISH =  '[A-Za-z' + ''.join(['%s-%s' % (s,e) for s,e in CHINESE_UNICODE]) + ']'
+
+def clean_conju(conjugation_list : List[str]):
+  output = list(filter(lambda x :j2hcj(h2j(x['long']))[-1] in EOMI, conjugation_list))
+  if len(output) == 0:
+    return ''
+  
+  else:
+    output = list(filter(lambda x: x != None, sum([list(x.values()) for x in output],[])))
+    output = sorted(output, key = lambda x : len(x)) 
+    return output[0] if len(output) > 0 else ''
